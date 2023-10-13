ede1477bb625dc177447a51a48bb6c6e85c32aa3
KazupD <kazup.daniel@gmail.com>
2023-10-13 20:55:07 +0200

    cart table finished

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -3,7 +3,9 @@ const cart_table = document.getElementById("cart_table");
 const total_price_head = document.getElementById("total_price");
 
 const clear_cart_button = document.getElementById("clear_cart");
+const finish_shopping_button = document.getElementById("finish_shopping");
 
+let items_in_cart;
 let total_price = 0;
 
 window.onload = async function (){
@@ -22,8 +24,52 @@ function removeAllRow() {
     }
 }
 
+function plusItem(id){
+    items_in_cart = JSON.parse(sessionStorage.getItem("cart"));
+    for (let i = 0; i < items_in_cart.length; i++) {
+        if(items_in_cart[i]["id"] === id){
+            Number(items_in_cart[i]["price_sum"]);
+            items_in_cart[i]["price_sum"] += Number(items_in_cart[i]["price"]);
+            Number(items_in_cart[i]["quantity"]);
+            items_in_cart[i]["quantity"] += 1;
+        }
+    }
+    sessionStorage.setItem("cart", JSON.stringify(items_in_cart));
+    removeAllRow();
+    refreshCartTable();
+}
+function minusItem(id){
+    items_in_cart = JSON.parse(sessionStorage.getItem("cart"));
+    for (let i = 0; i < items_in_cart.length; i++) {
+        if(items_in_cart[i]["id"] === id && Number(items_in_cart[i]["quantity"]) > 1){
+            Number(items_in_cart[i]["price_sum"]);
+            items_in_cart[i]["price_sum"] -= Number(items_in_cart[i]["price"]);
+            Number(items_in_cart[i]["quantity"]);
+            items_in_cart[i]["quantity"] -= 1;
+        }
+    }
+    sessionStorage.setItem("cart", JSON.stringify(items_in_cart));
+    removeAllRow();
+    refreshCartTable();
+}
+
+function dropItem(id){
+    items_in_cart = JSON.parse(sessionStorage.getItem("cart"));
+    let row_index;
+    for (let i = 0; i < items_in_cart.length; i++) {
+        if(items_in_cart[i]["id"] === id){
+            row_index = i;
+        }
+    }
+    items_in_cart.splice(row_index, 1);
+    sessionStorage.setItem("cart", JSON.stringify(items_in_cart));
+    cart_table.removeChild(cart_table.querySelector(`[id="${id}"]`));
+    removeAllRow();
+    refreshCartTable();
+}
+
 function refreshCartTable(){
-    let items_in_cart = JSON.parse(sessionStorage.getItem("cart"));
+    items_in_cart = JSON.parse(sessionStorage.getItem("cart"));
     if(items_in_cart === null){sessionStorage.setItem("cart", JSON.stringify([]));}
     total_price = 0;
     const table_head = document.createElement("tr");
@@ -46,7 +92,7 @@ function refreshCartTable(){
 
     for (let i = 0; i < items_in_cart.length; i++) {
 
-        total_price += Number(items_in_cart[i]["price"]);
+        total_price += Number(items_in_cart[i]["price_sum"]);
         const table_row = document.createElement("tr");
         let id = items_in_cart[i]["id"]
         table_row.id = id;
@@ -60,13 +106,13 @@ function refreshCartTable(){
             <td class="cart_item_common" id="cart_item_power${id}"></td>
             <td class="cart_item_important" id="cart_item_price${id}"></td>
             <td class="cart_item_handle" id="cart_item_plus${id}">
-                <button class="handle_button" id="plus_button${id}">+1</button>
+                <button class="handle_button" id="plus_button${id}" onclick="plusItem(${id})">+1</button>
             </td>
             <td class="cart_item_handle" id="cart_item_minus${id}">
-                <button class="handle_button" id="minus_button${id}">+1</button>
+                <button class="handle_button" id="minus_button${id}" onclick="minusItem(${id})">-1</button>
             </td>
             <td class="cart_item_handle" id="cart_item_drop${id}">
-                <button class="handle_button" id="drop_button${id}">Drop</button>
+                <button class="handle_button" id="drop_button${id}" onclick="dropItem(${id})">Drop</button>
             </td>
         `;
 
@@ -75,7 +121,7 @@ function refreshCartTable(){
         table_row.querySelector(`#cart_item_brand${id}`).innerText = String(items_in_cart[i]["brand"]);
         table_row.querySelector(`#cart_item_type${id}`).innerText = String(items_in_cart[i]["type"]);
         table_row.querySelector(`#cart_item_power${id}`).innerText = String(items_in_cart[i]["power"]);
-        table_row.querySelector(`#cart_item_price${id}`).innerText = String(items_in_cart[i]["price"]);
+        table_row.querySelector(`#cart_item_price${id}`).innerText = String(items_in_cart[i]["price_sum"]);
 
         cart_table.appendChild(table_row);
 
